48213a88f12f9ff9b126e5477a87c31bf4679ffc
Matheus Alcantara <202010120030@ifba.edu.br>
2024-12-17 18:40:31 -0300

    adicionado o conversor de temperatura

--- main.c
+++ main.c
@@ -12,6 +12,30 @@ void volume(){
   
 }
 void temperatura(){
+  int acabou = 0;
+  float temperatura;
+  char escala;
+  printf("Digite o numero seguido de sua escala \n");
+  printf("C- Celsius \nF- Fahrenheit \nK- Kelvin \n");
+  printf("Por exemplor 27C, para indicar 27 graus celsius \n");
+  scanf("%f %c",&temperatura,&escala);
+  switch (escala) {
+    case 'C':
+      printf("%.2f em Celsius equivalem a %.2f em Fahrenheit e %.2f em Kelvin \n",temperatura,(temperatura * 9/5) + 32,temperatura + 273);
+      break;
+    case 'K':  
+      printf("%.2f em Kelvin equivalem a %.2f em Fahrenheit e %.2f em Celsius \n",temperatura,1.8 * (temperatura - 273) + 32,temperatura - 273);
+      break;
+    case 'F':  
+      printf("%.2f em Fahrenheit equivalem a %.2f em Kelvin e %.2f em Celsius \n",temperatura,(temperatura - 32) * 5/9 + 273,(temperatura - 32) / 1.8);
+      break;
+      
+    default:
+      system("cls");
+      printf("Unidade indisponivel no conversor\nVerifique se esta utilizando letras maiusculas\n");  
+      break;
+   }
+  
   
 }
 void velocidade(){
@@ -32,7 +56,7 @@ void bits(){
 int main(){
   int input = -1; 
   while (input != 0) {
-    printf("digite uma das opcoes: \n");
+    printf("Digite uma das opcoes: \n");
     printf("0- para fechar \n");
     printf("1- para converter unidades de comprimento \n");
     printf("2- para converter unidades de massa \n");
@@ -43,7 +67,13 @@ int main(){
     printf("7- para converter unidades de area \n");
     printf("8- para converter unidades de tempo \n");
     printf("9- para converter unidades de bits \n");
-    scanf("%d",&input);
+   if (scanf("%d", &input) != 1) {
+      // Limpa o buffer de entrada se a leitura falhar
+      while (getchar() != '\n');  // Limpa qualquer caractere restante
+      system("cls");
+      printf("Entrada invalida. Por favor, insira um numero entre 0 e 9.\n");
+      continue;  // Retorna ao início do loop
+    }
     switch (input) {
       case 0:
         input = 0;
@@ -78,7 +108,7 @@ int main(){
         break;
       default:
         system("cls");//limpa o terminal
-        printf("input invalido, tente novamente. \n");
+        printf("Input invalido, tente novamente. \n");
         break;
     
     }
